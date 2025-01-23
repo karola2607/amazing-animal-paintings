3f24f66202021d63ec74c0164125dd18d04e7a0c
Karolina Pastor <borek.karola@gmail.com>
2025-01-23 11:39:45 +0100

    Add checkout logic

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -30,4 +30,8 @@ export class CartViewComponent implements OnInit {
   clearCart(): void {
     this.cartService.clearCart().subscribe();
   }
+
+  checkout(): void {
+    this.cartService.checkout(this.cartItems).subscribe();
+  }
 }
